e768236e7108c7971744221bed9226a570df2225
Al Mobin <66089840+mobin-ism@users.noreply.github.com>
2022-07-24 18:19:21 +0600

    Interection with smart contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,14 +1,8 @@
 import { ethers, run, network } from "hardhat";
 
 async function main() {
-	const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-	const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-	const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-	const lockedAmount = ethers.utils.parseEther("0.00000001");
-
 	const Lock = await ethers.getContractFactory("Lock");
-	const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	const lock = await Lock.deploy();
 
 	await lock.deployed();
 
@@ -17,9 +11,15 @@ async function main() {
 	// Verify the contract was deployed correctly
 	if (31337 != network.config.chainId && process.env.ETHERSCAN_API_KEY) {
 		await lock.deployTransaction.wait(6);
-		await verify(lock.address, [unlockTime]);
-		console.log(lock.address);
+		await verify(lock.address, []);
 	}
+
+	// INTERACTING WITH THE CONTRACT
+	// Update the current value
+	const transactionResponse = await lock.store(71);
+	await transactionResponse.wait(1);
+	const updatedValue = await lock.retreive();
+	console.log(`Updated Value is: ${updatedValue}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -33,7 +33,6 @@ main().catch((error) => {
  * VERIFYING SMART CONTRACT DEPLOYMENT
  */
 async function verify(contractAddress: string, args: any[]) {
-	console.log(contractAddress);
 	try {
 		await run("verify:verify", {
 			address: contractAddress,
