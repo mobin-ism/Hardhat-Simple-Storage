9c5002ccd07b1222b1d2b7a046df233567b7a48f
Al Mobin <66089840+mobin-ism@users.noreply.github.com>
2022-07-25 12:36:09 +0600

    Solidity unit testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,12 +7,19 @@ const PRIVATE_KEY: string = process.env.PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
 	solidity: "0.8.9",
-	defaultNetwork: "hardhat",
+	defaultNetwork: "localhost",
 	networks: {
 		rinkeby: {
 			url: process.env.RINKEBY_RPC_URL,
 			accounts: [PRIVATE_KEY],
 			chainId: 4,
+		},
+		localhost: {
+			url: "http://127.0.0.1:8545",
+			accounts: [
+				"0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+			],
+			chainId: 31337,
 		},
 	},
 	etherscan: {
